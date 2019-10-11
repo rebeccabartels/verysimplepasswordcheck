06f6f6a53caee263a9b2dc2a374378bcd3d80c66
Rebecca Bartels <rebeccakbartels@gmail.com>
2019-10-10 20:29:41 -0400

    Create drinkingage.py

---
+++ drinkingage.py
@@ -0,0 +1,8 @@
+drinking_age = str(21)
+
+whats_age = str(input("please enter your age here: "))
+
+if (whats_age <= drinking_age):
+    print('You have been denied')
+elif (whats_age >= drinking_age):
+    print('DRINK AWAY!!')
